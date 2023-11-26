acd2d2758a3b5ed540dba0917da63fe5059ed8b6
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 22:47:17 +0000

    Add game_model.dart

---
+++ lib/models/game_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class GameModel {
+  // This class represents the game state and logic.
+  // Add attributes and methods to control the game play and tracking.
+}
