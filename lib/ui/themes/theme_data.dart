8b32f9f47616f3cb1239399e4496719866e50a73
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 22:47:03 +0000

    Add theme_data.dart

---
+++ lib/ui/themes/theme_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeData {
+  // This class houses theme data to be used across the app.
+  // You can add different themes and switch between them as required.
+  // Example theme:
+  // static final ThemeData defaultTheme = ThemeData(
+  //    primaryColor: Colors.blue,
+  //    barColor: Colors.white,
+  //    fontFamily: 'Sanserif',
+  //    buttonTheme: ButtonThemeData(textTheme: TextTheme(...defaultTextTheme)),
+  // );
+}
