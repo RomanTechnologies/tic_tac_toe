8ccd764b3a7d10138796f8780b4a44c715a8e22a
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:01:01 +0000

    Add game_viewmodel.dart

---
+++ lib/viewmodels/game_viewmodel.dart
@@ -0,0 +1,8 @@
+import 'package:stacked/stacked.dart';
+import '../models/game_model.dart';
+import '../services/game_service.dart';
+
+class GameViewModel extends BaseViewModel {
+  GameModel?gameModel;
+  // Add functionalities and logic to manage the game state.
+}
