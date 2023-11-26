8bd2d1d56ad475ba8b204422b803077bf8a551e7
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:20:06 +0000

    Add game_mode_service.dart

---
+++ lib/services/game_mode_service.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../models/game_model.dart';
+
+class GameModeService {
+  // This service handles game mode logic and interactions.
+  // Add methods to manage game modes and settings.
+}
