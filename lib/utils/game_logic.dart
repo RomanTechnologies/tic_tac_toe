11a72a30dd6d0fcf96e77a08e0daefd086c8ecf3
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 22:52:36 +0000

    Add game_logic.dart

---
+++ lib/utils/game_logic.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../models/game_model.dart';
+
+class GameLogic {
+  // This class handles the logic of the Tic Tac Toe game.
+
+  static void playGame(GameModel gameModel) {
+    // Add your game logic here.
+  }
+}
