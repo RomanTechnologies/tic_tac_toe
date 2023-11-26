6f94b7e8d3b00a0e4eddbe65d635f79ebcd15823
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 22:48:53 +0000

    Add firebase_service.dart

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class FirebaseService {
+  // This class provides all Firebase related services.
+  // Methods for authentication, realtime database and other Firebase functionalities.
+}
