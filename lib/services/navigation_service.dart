900333b3795f8ad5a5e9a747b7219670757e6eca
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:01:40 +0000

    Add navigation_service.dart

---
+++ lib/services/navigation_service.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../utils/router.dart';
+
+import 'package:stacked_services/stacked_services.dart';
+
+class NavigationService extends BaseNavigationService {}
