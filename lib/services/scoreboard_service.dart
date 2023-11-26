9789d830e936aaff06db1540a87541be12386acb
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:12:50 +0000

    Add scoreboard_service.dart

---
+++ lib/services/scoreboard_service.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../models/game_model.dart';
+
+class ScoreboardService {
+  // This service handles the scoreboard logic and interactions.
+  // Add methods to manage scores and player statistics.
+}
