b8b5cf3f7e90cabd3c0bd94185cabf5c8cef6347
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:02:35 +0000

    Add constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+// This file contains constants and enums used across the app.
+
+const double gameBoardSize = 300.; // Size of the game board.
+const int defaultBoardSize = 4; // Default size of the game board.
+const double marginSize = 10; // Margin for the game board.
+
+enum GameMode {
+  playerVsComputer,
+  twoPlayers,
+  onlinePlay,
+}
