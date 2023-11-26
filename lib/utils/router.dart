2573dfc709432d70cad7e8c966141737e32f5b06
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-26 23:02:08 +0000

    Add router.dart

---
+++ lib/utils/router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foundation/flutter_foundation.dart';
+
+import '../ui/views/home_view.dart';
+import '../ui/views/game_board_view.dart';
+
+import '../ui/views/settings_view.dart';
+
+Router createRouter() {
+  return Router(
+    onGenerateRoute: (RouteSettings settings) {
+      // Add route handling logic here.
+    },
+  );
+}
