4575d9cebfee1e2901cf4779f315fe286125bb32
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-27 01:10:07 +0000

    Update main.dart with routing and theme setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'utils/router.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  widgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase and other services
+  runApp(App(router: createRouter()));
 }
