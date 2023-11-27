594d9fca222f9e55cf1364100ee1cee3b497e4fd
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-27 01:15:27 +0000

    Update home_view.dart with complete implementation

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import '../themes/theme_data.dart';
+import '../../viewmodels/game_viewmodel.dart';
+import '../services/navigation_service.dart';
 
 class HomeView extends StatelessWidget {
-  HomeView({Key? key}) : super(key: key);
+  final GameViewModel _viewModel;
+  final NavigationService _navigationService;
+
+  HomeView({Key? key, required GameViewModel viewModel, required NavigationService navigationService})
+      : _viewModel = viewModel, _navigationService = navigationService,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tic Tac Toe'),
-      ),
-      body: Center(
-        child: Text('Welcome to Tic Tac Toe!'),
+      appBar: AppBar(title: Text('Tic Tac Toe')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Welcome to Tic Tac Toe!')].add(FlatButton(
+            onPressed: () {_viewModel.navigateToGame();},
+            child: Text('Play Game'),
+          )),
       ),
     );
   }
