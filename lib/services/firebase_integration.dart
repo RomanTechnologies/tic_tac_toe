31c2cfef072503979980c8907dbd285838c4d0df
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-27 01:04:47 +0000

    Add firebase_integration.dart

---
+++ lib/services/firebase_integration.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseIntegration {
+  // This class integrates all Firebase services.
+  // Includes authentication, database, and storage services.
+}
