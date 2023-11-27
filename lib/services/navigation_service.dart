4bc1e833aa7a4da879d2488c9c5468692c867678
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-27 01:14:49 +0000

    Update navigation_service.dart with complete implementation

--- lib/services/navigation_service.dart
+++ lib/services/navigation_service.dart
@@ -1,6 +1,24 @@
+
 import 'package:flutter/material.dart';
 import '../utils/router.dart';
-
 import 'package:stacked_services/stacked_services.dart';
 
-class NavigationService extends BaseNavigationService {}
+/**
+ * Custom navigation service using the Stacked package.
+ * This class handles app navigation logic. 
+ */
+class NavigationService extends BaseNavigationService {
+  // Global key for the navigator state.
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  // Get the current context of the navigator.
+  BuildContext? get currentContext => navigatorKey.currentState?.context;
+
+  // Navigate to a new route.
+  void toRoute(String routeName) {
+    navigatorKey.currentState?.pushNamed(routeName);
+  }
+
+  // Navigate back.
+  void back() => navigatorKey.currentState?.pop();
+}
