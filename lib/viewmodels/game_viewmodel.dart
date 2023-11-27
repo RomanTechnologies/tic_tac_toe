9d1d226011cf505eee901206950f0404471e30b7
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2023-11-27 01:14:03 +0000

    Update game_viewmodel.dart with full implementation

--- lib/viewmodels/game_viewmodel.dart
+++ lib/viewmodels/game_viewmodel.dart
@@ -1,8 +1,16 @@
 import 'package:stacked/stacked.dart';
 import '../models/game_model.dart';
 import '../services/game_service.dart';
+import '../services/navigation_service.dart';
 
 class GameViewModel extends BaseViewModel {
-  GameModel?gameModel;
-  // Add functionalities and logic to manage the game state.
+  final GameService _gameService;
+  final NavigationService _navigationService;
+
+  GameViewModel({GameService? gameService, NavigationService? navigationService})
+      : _gameService = gameService || GameService(),
+        _navigationService = navigationService || NavigationService(),
+        super();
+
+  // Implement app logic and functions here
 }
